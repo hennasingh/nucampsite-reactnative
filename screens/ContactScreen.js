163ed83000a5ef58a8bb9af83bc234a12b04b3a3
Henna Singh <hennasingh04@gmail.com>
2023-06-03 16:21:54 +0100

    added contact details to contact screen

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import { ScrollView } from 'react-native-gesture-handler'
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 export default function ContactScreen() {
 
     return (
         <ScrollView>
-            
+            <Card wrapperStyle={{margin: 10}}>
+                <Card.Title>
+                    Contact Information
+                </Card.Title>
+                <Card.Divider />
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{marginBottom:10}}>U.S.A</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>           
+            </Card>
+
         </ScrollView>
     )
 }
