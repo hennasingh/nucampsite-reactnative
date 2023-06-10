80dde971123f51528bfd23491d8eeee8fd631307
Henna Singh <hennasingh04@gmail.com>
2023-06-10 16:13:10 +0100

    added pencil icon in campsite info screen and attached a modal to it

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -11,25 +11,32 @@ export default function RenderCampsite(props) {
                 <Card.Image source={{ uri: baseUrl + campsite.image}}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
-                            style={{
-                            color: 'white',
-                            textAlign: 'center',
-                            fontSize: 20
-                            }} >
+                            style={styles.cardText}>
                             {campsite.name}
                         </Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                <Icon
-                    name= {props.isFavorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    onPress={() => {
-                        props.isFavorite ? console.log('Already Favorite') : props.markFavorite()
-                    }}
-                 />
+                <View style={styles.cardRow}>
+                    <Icon
+                        name= {props.isFavorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        onPress={() => {
+                            props.isFavorite ? console.log('Already Favorite') : props.markFavorite()
+                        }}
+                    />
+                    <Icon 
+                        name='pencil'
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        onPress={() => {
+                            props.onShowModal()
+                        }}
+                    />
+                </View>
             </Card>
         )
     }
@@ -42,5 +49,20 @@ const styles = StyleSheet.create({
         padding:0,
         margin:0,
         marginBottom: 20
+    },
+    cardRow: {
+        alignItems:'center',
+        justifyContent:'center',
+        flex:1,
+        flexDirection:'row',
+        margin: 20
+    },
+    cardText: {
+        textShadowColor:'rgba(0, 0, 0, 1)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 20,
+        textAlign:'center',
+        color: 'white',
+        fontSize: 20
     }
 });
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,6 +1,6 @@
 import RenderCampsite from '../features/campsites/RenderCampsite'
 import React from 'react'
-import { FlatList, StyleSheet, Text, View} from 'react-native';
+import { FlatList, StyleSheet, Text, View, Button, Modal} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
 
@@ -10,6 +10,7 @@ export default function CampsiteInfoScreen({ route }) {
 
     const comments = useSelector((state) => state.comments)
     const [favorite, setFavorite] = React.useState(false);
+    const [showModal, setShowModal] = React.useState(false);
 
     const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
@@ -24,23 +25,44 @@ export default function CampsiteInfoScreen({ route }) {
         )
     }
     return (
-        <FlatList 
-            data={ comments.commentsArray.filter(
-                (comment) => comment.campsiteId === campsite.id
-            )}
-            renderItem={renderCommentItem}
-            keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20}}
-            ListHeaderComponent= {
-                <>
-                    <RenderCampsite 
-                    campsite={campsite}
-                    isFavorite= {favorites.includes(campsite.id)} 
-                    markFavorite= {() => dispatch(toggleFavorite(campsite.id))}/>
-                    <Text style={styles.commentsTitle}>Comments</Text>
-                </>
-            }
-        />
+        <>
+            <FlatList 
+                data={ comments.commentsArray.filter(
+                    (comment) => comment.campsiteId === campsite.id
+                )}
+                renderItem={renderCommentItem}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{ marginHorizontal: 20, paddingVertical: 20}}
+                ListHeaderComponent= {
+                    <>
+                        <RenderCampsite 
+                        campsite={campsite}
+                        isFavorite= {favorites.includes(campsite.id)} 
+                        markFavorite= {() => dispatch(toggleFavorite(campsite.id))}
+                        onShowModal={() => setShowModal(!showModal)}
+                        />
+                        <Text style={styles.commentsTitle}>Comments</Text>
+                    </>
+                }
+            />
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <View style={{margin: 10}}>
+                        <Button 
+                            onPress={() => setShowModal(!showModal)} 
+                            color='#808080'
+                            title='Cancel'
+                        />
+                    </View>                  
+                </View>
+            </Modal>
+        </>
+        
     )
 }
 
@@ -58,6 +80,10 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
+    },
+    modal: {
+        justifyContent:'center',
+        margin:20
     }
 
 })
