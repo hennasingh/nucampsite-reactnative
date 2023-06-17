44742dbfcf18738f6571200d38c2187e5dd4997a
Henna Singh <hennasingh04@gmail.com>
2023-06-17 09:54:06 +0100

    implemented persistence via redux persist

--- redux/store.js
+++ redux/store.js
@@ -4,13 +4,45 @@ import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { favoriteReducer } from '../features/favorites/favoritesSlice';
+import {
+    persistStore,
+    persistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const config = {
+    key: 'root',
+    storage: AsyncStorage,
+    debug: true
+}
 
 export const store = configureStore({
-    reducer: {
-        campsites: campsitesReducer,
-        comments: commentsReducer,
-        partners: partnersReducer,
-        promotions: promotionsReducer,
-        favorites: favoriteReducer
-    }
+        reducer: persistCombineReducers(config, {
+            campsites: campsitesReducer,
+            comments: commentsReducer,
+            partners: partnersReducer,
+            promotions: promotionsReducer,
+            favorites: favoriteReducer
+        }),
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [
+                FLUSH,
+                REHYDRATE,
+                PAUSE,
+                PERSIST,
+                PURGE,
+                REGISTER
+            ]
+        }
+    })
 });
+
+export const persistor = persistStore(store);
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,2 @@
-//export const baseUrl = 'http://192.168.0.157:3001/';
-export const baseUrl='http://10.77.0.170:3001/';
+export const baseUrl = 'http://192.168.0.157:3001/';
+//export const baseUrl='http://10.77.0.170:3001/';
