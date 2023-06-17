0b0acf9c3185424c00275ff4d8e4d793fe962be3
Henna Singh <hennasingh04@gmail.com>
2023-06-17 16:05:14 +0100

    replace modal with alert in the reservation screen

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -71,10 +71,10 @@ export default function CampsiteInfoScreen({ route }) {
                 ListHeaderComponent= {
                     <>
                         <RenderCampsite 
-                        campsite={campsite}
-                        isFavorite= {favorites.includes(campsite.id)} 
-                        markFavorite= {() => dispatch(toggleFavorite(campsite.id))}
-                        onShowModal={() => setShowModal(!showModal)}
+                            campsite={campsite}
+                            isFavorite= {favorites.includes(campsite.id)} 
+                            markFavorite= {() => dispatch(toggleFavorite(campsite.id))}
+                            onShowModal={() => setShowModal(!showModal)}
                         />
                         <Text style={styles.commentsTitle}>Comments</Text>
                     </>
