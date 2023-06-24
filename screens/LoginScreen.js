6ea5578cab496cac0b2270699d13068183fcae63
Henna Singh <hennasingh04@gmail.com>
2023-06-24 16:11:46 +0100

    added image manipulator to resize camera image before uploading as profile image

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import * as ImagePicker from 'expo-image-picker';
 import { baseUrl } from '../shared/baseUrl';
 import logo from '../assets/images/logo.png';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 
 function LoginTab({ navigation }) {
@@ -150,9 +151,21 @@ const RegisterTab = () => {
             });
             if (capturedImage.assets) {
                 console.log(capturedImage.assets[0]);
-                setImageUrl(capturedImage.assets[0].uri);
+                processImage(capturedImage.assets[0].uri);
             }
         }
+    }
+
+    const processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [{resize:{width:400}}],
+            {format: ImageManipulator.SaveFormat.PNG}
+        );
+        
+        console.log(processedImage)
+        setImageUrl(processedImage.uri)
+
     }
 
     return (
