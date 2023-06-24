a658a6408c68c02650021a0b8a14763d354df922
Henna Singh <hennasingh04@gmail.com>
2023-06-24 11:09:29 +0100

    added login and register components on LoginScreen using tab navigation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -18,6 +18,7 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import FavoritesScreen from './FavoritesScreen';
 import LoginScreen from './LoginScreen';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/core';
 
 
 const Drawer = createDrawerNavigator();
@@ -134,10 +135,15 @@ const LoginNavigator = () => {
             <Stack.Screen 
                 name='Login'
                 component = { LoginScreen }
-                options={({ navigation }) => ({ 
+                options={({ navigation, route }) => ({ 
+                    headerTitle: getFocusedRouteNameFromRoute(route),
                     headerLeft: () => (
                         <Icon
-                            name='sign-in'
+                            name= {
+                                getFocusedRouteNameFromRoute(route) === 'Register'
+                                    ? 'user-plus'
+                                    : 'sign-in'
+                            }
                             type='font-awesome'
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
